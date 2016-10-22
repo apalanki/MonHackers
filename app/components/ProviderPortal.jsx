eef526fa6e09cbd0af982d7241e1badf6fe1d952
kwolt <kara.wolter@monsanto.com>
2016-10-22 18:17:38 -0500

    migrating login to a modal

--- app/components/ProviderPortal.jsx
+++ app/components/ProviderPortal.jsx
@@ -1,57 +1,47 @@
 import React from 'react';
 import {Form, FormControl, FormGroup, Checkbox, Button, Col, ControlLabel }from 'react-bootstrap';
-
+import LoginModal from './LoginModal.jsx';
 
 const ProviderPortal = React.createClass({
     getInitialState(){
         return {
-            isLoggedIn: false
+            isLoggedIn: false,
+            showLogInModal: true
         };
     },
     setLoggedIn() {
         this.setState({isLoggedIn:true});
     },
+    openLoginModal() {
+        this.setState({showLogInModal: true});
+    },
+
+    closeLoginModal(successful) {
+        this.setState({showLogInModal: false});
+        if(!successful) {
+            document.location.href='/';
+        }
+    },
+    renderLoginModal() {
+        return (
+            <LoginModal
+                show={this.state.showLogInModal}
+                close={this.closeLoginModal} />
+        );
+    },
     render(){
-        if(!this.state.isLoggedIn){
-            return(
-                <div className="form-container">
-                    <form className='form-padding'>
-                        <div className='row'>
-                            <div className='col-md-6 col-md-offset-3'>
-                                <h3>Log in to your account:</h3>
-                            </div>
-                        </div>
-                        <div className='row'>
-                            <div className='col-md-3 col-md-offset-3'>
-                                <FormGroup controlId='email'>
-                                    <ControlLabel>Email</ControlLabel>
-                                    <FormControl type='email' placeholder="Email"/>
-                                </FormGroup>
-                            </div>
-                            <div className='col-md-3'>
-                                <FormGroup controlId='password'>
-                                    <ControlLabel>Password</ControlLabel>
-                                    <FormControl type='password' placeholder="Password" />
-                                </FormGroup>
-                            </div>
-                        </div>
-                        <div className='row'>
-                            <div className='col-md-3 col-md-offset-8'>
-                                <Button id='login' bsStyle='primary'
-                                    onClick={this.setLoggedIn}>
-                                    Login
-                                </Button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            );
-        }
-        else{
         return(
-            <div></div>
-            );
-        }
+            <div className="form-container">
+                {this.renderLoginModal()}
+                <form className='form-padding'>
+                    <div className='row'>
+                        <div className='col-md-6 col-md-offset-3'>
+                            <h3>TESTING</h3>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
     }
 });
 
