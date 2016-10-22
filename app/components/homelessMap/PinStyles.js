088774ac0233f4934877dcdb523bc472fbe0ac81
Valerie Houseman <vhouseman@climate.com>
2016-10-22 17:20:50 -0500

    Pin styles for eligible/ineligible

--- app/components/homelessMap/PinStyles.js
+++ app/components/homelessMap/PinStyles.js
@@ -1,7 +1,9 @@
 const K_WIDTH = 40;
 const K_HEIGHT = 28;
 
-const circleStyle = function (color) {
+const circleStyle = function (eligible) {
+    var border_color = eligible ? '#337ab7' : '#808080'; 
+    var bg_color = eligible ? 'white' : '#D3D3D3';
     return {
 
         position: 'absolute',
@@ -10,9 +12,9 @@ const circleStyle = function (color) {
         left: -K_WIDTH / 2,
         top: -K_HEIGHT / 2,
 
-        border: `5px solid ${color}`,
+        border: `5px solid ${border_color}`,
         borderRadius: K_HEIGHT,
-        backgroundColor: 'white',
+        backgroundColor: bg_color,
         textAlign: 'center',
         fontSize: 10,
         fontWeight: 'bold',
