4b3443b31e5a912a290e042ebb96c0addbe38b2f
Valerie Houseman <vhouseman@climate.com>
2016-10-22 16:46:55 -0500

    Recenter the map

--- app/components/homelessMap/HomelessMap.jsx
+++ app/components/homelessMap/HomelessMap.jsx
@@ -25,13 +25,12 @@ let HomelessMap = React.createClass({
             return (<ShelterPin key={index} lat={shelter.latitude} lng={shelter.longitude}
                                 missing_requirements={shelter.missing_requirements} text={shelter.capacity}/>);
         });
-    },
     render() {
         return (
             <div className="map-wrapper">
                 <GoogleMap
                     bootstrapURLKeys={{key: MapsApiKey}}
-                    center={[38.637607, -90.1848]}
+                    center={[38.668104, -90.335153]}
                     zoom={12}>
                     {this.getMarkers()}
                 </GoogleMap>
