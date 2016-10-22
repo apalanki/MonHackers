d7293258374108cf35a1b1b3b6fa0ecec03aace9
Anudeep Palanki <palankianudeep@gmail.com>
2016-10-22 15:39:56 -0500

    adding plivo end point for texting

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const log4js = require('log4js');
 const logger = log4js.getLogger();
 
 const bodyParser = require('body-parser');
+var plivo = require('plivo');
 
 const port = 5000;
 const appBaseUrl = '/gh6';
@@ -18,8 +19,29 @@ app.get('/*', (req, res) => {
   res.sendFile((path.join(__dirname, '../deploy/index.html')), {title: 'GlobalHack VI' });
 });
 var server = app.listen(port, function() {
-
     logger.info('Started listening on port', port);
+});
+
+app.all('/reply_to_sms/', function(request, response) {
+    // Sender's phone number
+    var from_number = request.body.From || request.query.From;
+    // Receiver's phone number - Plivo number
+    var to_number = request.body.To || request.query.To;
+    // The text which was received
+    var text = request.body.Text || request.query.Text;
+
+    var params = {
+        'src' : to_number,
+        'dst' : from_number
+    };
+    var body = "Thanks, we've received your message.";
+
+    var r = plivo.Response();
+    r.addMessage(body, params);
+    console.log (r.toXML());
+
+    response.set({'Content-Type': 'text/xml'});
+    response.end(r.toXML());
 });
 
 const io = require('socket.io')(server);
