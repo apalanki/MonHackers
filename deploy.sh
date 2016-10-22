49399241cec6d1bd260ab66b7a9630a4d595c377
Anudeep Palanki <palankianudeep@gmail.com>
2016-10-21 23:03:47 -0500

    Adding circle.yml script to enable circle deploys

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-echo "adding known hosts"
-echo -e "Host 159.203.169.240\n\tHostName 159.203.169.240\n\tUser dokku\n\tStrictHostKeyChecking no\n\tIdentityFile ~/.ssh/id_rsa\n" >> ~/.ssh/config
+#echo "adding known hosts"
+#echo -e "Host 159.203.169.240\n\tHostName 159.203.169.240\n\tUser dokku\n\tStrictHostKeyChecking no\n\tIdentityFile ~/.ssh/id_rsa\n" >> ~/.ssh/config
 echo "adding dokku remote"
 git remote add dokkuRemote dokku@159.203.169.240:it_homelessness
 echo "pushing master to dokku"
