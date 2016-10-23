b08829d3169deff84fa571b1248444ce2b31ec9d
Anudeep Palanki <palankianudeep@gmail.com>
2016-10-22 23:28:18 -0500

    popovers on google map pins

--- app/components/homelessMap/HomelessMap.jsx
+++ app/components/homelessMap/HomelessMap.jsx
@@ -14,7 +14,7 @@ let HomelessMap = React.createClass({
     shouldComponentUpdate: shouldPureComponentUpdate,
     getMarkers(){
         return this.props.shelters.map(function (shelter, index) {
-            return (<ShelterPin key={index} lat={shelter.latitude} lng={shelter.longitude}
+            return (<ShelterPin key={index} lat={shelter.latitude} shelter={shelter} lng={shelter.longitude}
                                 missing_requirements={shelter.missing_requirements} text={shelter.capacity_available}/>);
         });
     },
--- app/components/homelessMap/ShelterPin.jsx
+++ app/components/homelessMap/ShelterPin.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
 let styles = require('./PinStyles');
+import {Popover} from 'react-bootstrap';
 
 var ShelterPin = React.createClass({
+    propTypes: {
+        shelter: React.PropTypes.object
+    },
     getPinStatus(){
-        var incomplete = (this.props.missing_requirements && 
-            this.props.missing_requirements.length !== 0);
+        var incomplete = (this.props.missing_requirements &&
+        this.props.missing_requirements.length !== 0);
         return !incomplete;
     },
+    getInitialState(){
+        return {
+            modalIsOpen: false
+        }
+    },
+    openModal(){
+        this.setState({modalIsOpen: true});
+    },
+    closeModal(){
+        this.setState({modalIsOpen: false});
+    },
     render() {
+        const popover = (
+            <Popover id="modal-popover" title="popover">
+                very popover. such engagement
+            </Popover>
+        );
+
         return (
-            <div style={styles.circleStyle(this.getPinStatus())}>
-                {this.props.text}
+            <div>
+                <div onMouseEnter={this.openModal}
+                     onMouseLeave={this.closeModal}
+                     style={styles.circleStyle(this.getPinStatus())}>
+                    {this.props.text}
+                </div>
             </div>
         );
     }
