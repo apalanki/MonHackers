312d14ca432b24166445ff3f12c65520f4814f98
kwolt <kara.wolter@monsanto.com>
2016-10-23 04:38:37 -0500

    reformatting the landing page

--- app/components/LandingPage.jsx
+++ app/components/LandingPage.jsx
@@ -7,31 +7,29 @@ export default class LandingPage extends React.Component{
 			<div className="cards cards-info">
 				<div className="card card-hover">
 					<div className="card-header">
+						<Link to="homeless_map"><h3>I Need Help</h3></Link>
+					</div>
+					<img src = "./INeedHelp.png" />
+					<div className="card-block">
+						<p className="card-text" >Search our <Link to="homeless_map">map</Link> to find services in your area that meet your needs or fill out a Perfect Circle <Link to="application">profile</Link> to receive more individualized suggestions.</p>
+					</div>
+				</div>
+				<div className="card card-hover">
+					<div className="card-header">
+						<h3><Link to="volunteers">I Want to Help</Link></h3>
+					</div>
+					<img src = "./volunteer-icon.png" />
+					<div className="card-block">
+						<p className="card-text" >Do you have some free time, a special skill, or resources to help out? Help us to help someone in need. You have more to offer than you imagine and you could help change someones life.</p>
+					</div>
+				</div>
+				<div className="card card-hover">
+					<div className="card-header">
 						<Link to="provider"><h3>I'm a Service Provider</h3></Link>
 					</div>
 					<img src = "./provider_icon.png" />
 					<div className="card-block">
-						<p className="card-text" >Log in and view your service provider portal and manage your clients.</p>
-					</div>
-				</div>
-
-				<div className="card card-hover">
-					<div className="card-header">
-						<Link to="homeless_map"><h3>I Need Assistance</h3></Link>
-					</div>
-					<img src = "./INeedHelp.png" />
-					<div className="card-block">
-						<p className="card-text" ><Link to="homeless_map">Find services near you</Link> that meet your needs or <Link to="application">apply for services.</Link></p>
-					</div>
-				</div>
-
-				<div className="card card-hover">
-					<div className="card-header">
-						<h3><Link to="volunteers">I Want to Offer Help</Link></h3>
-					</div>
-					<img src = "./volunteer-icon.png" />
-					<div className="card-block">
-						<p className="card-text" >Help someone in need. You have more to offer than you think.</p>
+						<p className="card-text" >Manager your services through your provider portal. Allocate resources and help clients find the help they need.</p>
 					</div>
 				</div>
 			</div>
